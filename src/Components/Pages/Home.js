58d3cd9ad270d4d13d819b3774f51193a2376342
russelldnn <russelldnn@gmail.com>
2022-08-09 16:43:42 -0500

    home page roughly finished

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -24,6 +25,7 @@ import image12 from "../../assets/images/image12.jpg";
 
 
 export default function Home() {
+    
     return (
         <div>
             <div className="container">      
@@ -63,15 +65,71 @@ export default function Home() {
                     
                 <div className="locationDiv">
                     <ul>
-                        <li>Gallery 3 Bedroom</li>
-                        <li>Gallery 2 Bedroom</li>
-                        <li>Upper Deck 1 Bedroom</li>
-                        <li>Upper Deck Studio</li>
-                        <li>Sea Castles 2 Bedroom</li>
-                        <li>Villa</li>
-                        <li>Kensington</li>
-                        <li>Trafalgar</li>
+                        <li>
+                            <a 
+                            href="https://www.airbnb.com/rooms/48233576?guests=1&adults=1&s=67&unique_share_id=62047bf2-b05f-4343-b94d-22a8af86dae8"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Gallery 3 Bedroom
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                            href="https://www.airbnb.com/rooms/48167227?guests=1&adults=1&s=67&unique_share_id=9bf5f1e6-ddb9-4ab9-b9b7-964217dc4531"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Gallery 2 Bedroom
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                            href="https://www.airbnb.com/rooms/48077619?guests=1&adults=1&s=67&unique_share_id=e30e73e3-af85-4f81-a703-a4ae25e2642f"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Upper Deck 1 Bedroom</a>
+                        </li>
+                        <li>
+                            <a 
+                            href="https://www.airbnb.com/rooms/48075457?guests=1&adults=1&s=67&unique_share_id=a6215b3d-bb2e-4511-a115-c3992d8d4342"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Upper Deck Studio
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                            href="https://www.airbnb.com/rooms/50765039?guests=1&adults=1&s=67&unique_share_id=497ffe25-15b3-4e7b-ab2b-15bcf624190e"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Sea Castles 2 Bedroom
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                            href="https://www.airbnb.com/rooms/40650805?guests=1&adults=1&s=67&unique_share_id=2e8795be-3b3b-4d55-b3d9-506e123fb5ee"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Reflections Villa
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                            href="https://www.airbnb.com/rooms/48518376?guests=1&adults=1&s=67&unique_share_id=9485aaf0-ed1b-4288-8b6d-04b68f780855"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Kensington 1 Bedroom
+                            </a>
+                        </li>
+                        <li>
+                            <a 
+                            href="https://www.airbnb.com/rooms/48918108?guests=1&adults=1&s=67&unique_share_id=102db622-fe43-485d-9d71-203552da3e24"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Trafalgar 2 Bedroom
+                            </a>
+                        </li>
                     </ul>
+                    
                 </div>
                 
             </div>
