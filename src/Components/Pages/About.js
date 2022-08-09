29c147baf5b64ad997064bb6da00382b75cbf739
russelldnn <russelldnn@gmail.com>
2022-08-09 17:54:05 -0500

    about page responsive to size changes via flex

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,17 +1,21 @@
 import React from "react";
-import image13 from "../../assets/images/image13.jpg";
+
 
 export default function About() {
     return (
         <div>
-            <h1> About us</h1>
-            <div className="imageDiv">
-                <img src={image13} alt="pool" className="aboutPicture"/>
+            <div className="aboutTitle">
+                <h1> About Reflections</h1>
             </div>
-            <div className="AboutDiv">
-                <p>
-                    About us
-                </p>
+            <div className="container">
+            <div className="imageDiv">
+            
+                <div className="aboutDiv">
+                    <p>
+                    About us text
+                    </p>
+                </div>
+            </div>
             </div>
         </div>
     );
